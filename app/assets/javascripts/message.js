994c83cff3c69b8e3090c8f873def05b7909ebd4
misatone1989 <misa1201dasu@yahoo.co.jp>
2020-03-15 17:25:08 +0900

    add-automatic-function

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -3,7 +3,7 @@ $(function(){
     function buildHTML(message){
         if ( message.image ) {
             var html =
-            `<div class="message">
+            `<div class="message" data-message-id= ${message.id} >
                 <div class="main_chat__message__list__contents">
                 <div class="main_chat__message__list__user__name">
                 ${message.user_name}
@@ -23,7 +23,7 @@ $(function(){
             return html;
         } else {
             var html =
-            `<div class="message">
+            `<div class="message" data-message-id= ${ message.id } >
              <div class="main_chat__message__list__contents">
              <div class="main_chat__message__list__user__name">
              ${message.user_name}
@@ -39,33 +39,67 @@ $(function(){
              </div>
              </div>`
             return html;
-        };
-    }
+        }
+    };
 
   $('#new_message').on('submit', function(e){
-      e.preventDefault();
-      var formData = new FormData(this);
-      var url = $(this).attr('action')
-      $.ajax({
-        url: url,
-        type: "POST",
-        data: formData,
-        dataType: 'json',
-        processData: false,
-        contentType: false
-      })
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+    url: url,
+    type: "POST",
+    data: formData,
+    dataType: 'json',
+    processData: false,
+    contentType: false
+    })
 
-       .done(function(data){
-         var html = buildHTML(data);
-         $('.main_chat__message__list').append(html);  
-         $('.main_chat__message__list').animate({ scrollTop: $('.main_chat__message__list')[0].scrollHeight});    
-         $('form')[0].reset();
-         $('.form__submit').prop('disabled', false);
-        })
+    .done(function(data){
+        var html = buildHTML(data);
+        $('.main_chat__message__list').append(html);  
+        $('.main_chat__message__list').animate({ scrollTop: $('.main_chat__message__list')[0].scrollHeight});    
+        $('form')[0].reset();
+        $('.form__submit').prop('disabled', false);
+    })
 
-       .fail(function() {
-            alert("メッセージ送信に失敗しました");
-            $('.form__submit').prop('disabled', false);
-       })
+    .fail(function() {
+        alert("メッセージ送信に失敗しました");
+        $('.form__submit').prop('disabled', false);
+    });
   })
-})  
+  
+
+// ここから自動更新
+  var reloadMessages = function() {
+    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+    var last_message_id = $('.message:last').data("message-id");
+    $.ajax({
+      //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
+      url: "api/messages",
+      //ルーティングで設定した通りhttpメソッドをgetに指定
+      type: 'get',
+      dataType: 'json',
+      //dataオプションでリクエストに値を含める
+      data: {id: last_message_id}
+      })
+      .done(function(messages) {
+        if (messages.length !== 0)
+          //追加するHTMLの入れ物を作る
+          var insertHTML = '';
+          //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+          $.each(messages, function(i, message) {
+            insertHTML += buildHTML(message);
+          });
+          //メッセージが入ったHTMLに、入れ物ごと追加
+          $('.main_chat__message__list').append(insertHTML);
+          $('.main_chat__message__list').animate({ scrollTop: $('.main_chat__message__list')[0].scrollHeight});
+       })
+        .fail(function() {
+          alert('error');
+        });
+  }
+    if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+        setInterval(reloadMessages, 7000);
+    }
+});
